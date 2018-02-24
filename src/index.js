1a45d2aa8b978723a5cb2068fed9ecb280289bc9
ChernyshovaAlexandra <alexandarchrystal@gmail.com>
2018-02-24 20:21:22 +0300

    implementation of getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+    var fives = 0;
+
+
+    for(var i = 1; i <= number; i++){
+        if(i%5 === 0){
+            var yourNumber = i;
+            do{
+                fives+=1;
+                yourNumber/=5;
+            } while(yourNumber % 5 === 0)
+        }
+
+    }
+    return fives
+
+};
